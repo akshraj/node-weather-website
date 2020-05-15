d72d5f25d8b10ba94328252b3996bcb0a1851012
unknown <akshay raj singh>
2020-05-15 18:07:41 +0530

    remove console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('i am from client')
+
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.getElementById('message-one')
